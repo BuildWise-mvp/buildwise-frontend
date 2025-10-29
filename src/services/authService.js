2151cc8c737cfdd030ca2f4b0646f420e0696c5e
Marie Antoinette Houayek <marieantoinette.houayek@gmail.com>
2025-10-29 19:23:02 -0400

    Connected frontend to live backend

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://buildwise-systems-backend.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const loginUser = async (username, password) => {
   try {
--- src/services/projectService.js
+++ src/services/projectService.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { getAuthHeader } from "./authService";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export async function fetchProjects() {
   const headers = getAuthHeader();
