1fbbbdf15dc4146a839fbddb67a63d56a625a4d9
Marie Antoinette Houayek <marieantoinette.houayek@gmail.com>
2025-10-31 15:05:23 -0400

    Use Netlify /api proxy for dashboard endpoints

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { getAuthHeader } from "./authService";
 
-const API_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
+const API_URL = "/api";
 
 export async function fetchDashboardStats() {
   const headers = getAuthHeader();
